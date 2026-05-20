e5895c1eab8c065808b5721b7fa65ec0332604b6
formic-studio <kamil.k.kolodziejczyk@email.com>
2026-05-20 17:28:04 +0200

    xfactor

--- src/js/modules/thinktank-slider.js
+++ src/js/modules/thinktank-slider.js
@@ -23,7 +23,7 @@ const SLOT_PROFILE = {
       scale: 1,
     },
     2: {
-      xFactor: 3.0,
+      xFactor: 2.4,
       clipPath: "polygon(0 0, 74% 13%, 74% 87%, 0 100%)",
       overlayOpacity: 0.68,
       scale: 0.995,
@@ -43,7 +43,7 @@ const SLOT_PROFILE = {
       scale: 1,
     },
     2: {
-      xFactor: 3.0,
+      xFactor: 2.0,
       clipPath: "polygon(26% 13%, 100% 0, 100% 100%, 26% 87%)",
       overlayOpacity: 0.68,
       scale: 0.995,
